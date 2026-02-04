887940d97557f3dc1747ad3b27447117eb5981ff
Egor <arnoldtagirov1@gmail.com>
2026-02-04 19:52:48 +0100

    migrations ?

--- IM.Infrastructure/ApplicationDbContext.cs
+++ IM.Infrastructure/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-
+using IM.Domain;
 namespace IM.Infrastructure
 
 {
@@ -14,6 +14,12 @@ namespace IM.Infrastructure
         //public DbSet<HRDepartment> HRDepartments { get; set; }
         //public DbSet<FinanceDepartment> FinanceDepartments { get; set; }
         //public DbSet<SalaryRecord> SalaryRecords { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
+        public DbSet<PromoCode> Promocodes { get; set; }
+        public DbSet<User> Users {  get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     }
 }
